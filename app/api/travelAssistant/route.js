fece59ab10a975796ab34a452cf89f1460caf879
Matthew-Stormblessed <matthewisaacjohnson@gmail.com>
2026-07-02 16:24:58 -0600

    InitialCommit

---
+++ app/api/travelAssistant/route.js
@@ -0,0 +1,98 @@
+import { Agent, run, tool } from "@openai/agents";
+import { getJson } from "serpapi";
+import { z } from "zod";
+
+const getWeatherTool = tool({
+  name: "get_weather",
+  description: "Get the weather for a given latitude and longitude and how many forcast days.",
+  parameters: z.object({
+    latitude: z.string(),
+    longitude: z.string(),
+    forcastDays: z.string()
+  }),
+  async execute({ latitude, longitude, forcastDays }) {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&forcast_days=${forcastDays}`)
+
+    const data = await response.json();
+
+    return data;
+  },
+});
+
+const getHotelsTool = tool({
+  name: "get_hotels",
+  description: "Get the hotels for a given city, check in date, check out date, and number of adults.'",
+  parameters: z.object({
+    city: z.string(),
+    check_in: z.string(),
+    check_out: z.string(),
+    num_adults: z.string()
+  }),
+  async execute({ city, check_in, check_out, num_adults }) {
+
+    try {
+      const response = await getJson({
+        api_key: process.env.SERP_API_KEY,
+        engine: "google_hotels",
+        q: city,
+        check_in_date: check_in,
+        check_out_date: check_out,
+        adults: num_adults,
+        currency: "USD",
+        gl: "us",
+        hl: "en"
+      });
+
+      return response;
+    }
+    catch (e) {
+      console.log(e)
+    }
+  },
+});
+
+const getFlightsTool = tool({
+  name: "get_flights",
+  description: "Get flights from one location to another at a specific time. Uses google api. departure_id is the id of the airport you start from. And arrival_id is the id of the airport you end in. An example call might look like getFlightsTool('SLC', 'CUN', '2026-07-02', '2026-07-05')",
+  parameters: z.object({
+    departure_id: z.string(),
+    arrival_id: z.string(),
+    trip_start: z.string(),
+    trip_end: z.string()
+  }),
+  async execute({ departure_id, arrival_id, trip_start, trip_end }) {
+
+
+    const flightResponse = await getJson({
+      api_key: process.env.SERP_API_KEY,
+      engine: "google_flights",
+      hl: "en",
+      gl: "us",
+      departure_id: departure_id,
+      arrival_id: arrival_id,
+      outbound_date: trip_start,
+      return_date: trip_end,
+      currency: "USD"
+    })
+
+    return flightResponse
+  },
+});
+
+const agent = new Agent({
+  name: "Travel Agent",
+  instructions:
+    "You are a travel agent who gives concise recomendations. Use get_weather_tool as needed to fetch the weather of a location. Use get_flights tool to get flights for the specific request. No specific airport will be submitted, use the most popular one in that location. Do not ask follow up questions. When returning your final response, it will be in VALID JSON. The json will contain a weatherSummery(A 1 sentence summery of what the weather will look like in the destination at the time. If the start date is within 14 days of the current date use the get_weather tool. Otherwise just make a guess. Prefer to use the get_weather_tool though.), a flightSummery(a 1 sentence summery of what the flight will look like), and a flightLink(a link to the preffered flight. This link must include the departure and arrival). A hotel summery (1 sentence summerizing the results of using the get_hotels tool). And a hotel link(a link to the best hotel) If you do not know, dont make it up.",
+  model: "gpt-5.4-nano",
+  tools: [getWeatherTool, getFlightsTool, getHotelsTool]
+});
+
+export async function POST(request) {
+  const req = await request.json()
+
+  const now = new Date(); // Creates a Date object
+
+  const result = await run(agent, `Suggest what you would think is the best roundtrip flight from ${req.flyingFrom} to ${req.flyingTo} from ${req.startDate} to ${req.endDate} for ${req.numPeople} people and this price ${req.budget}. Also suggest a hotel in the area. This is the current date ${now}`);
+
+  return Response.json(result.finalOutput)
+}
--- app/page.js
+++ app/page.js
@@ -1,65 +1,125 @@
+
+'use client'
 import Image from "next/image";
+import Cat from "@/public/cat.png"
+import { useState } from "react"
 
 export default function Home() {
+  const [startScreen, setStartScreen] = useState(true)
+  const [numTravelers, setNumTravelers] = useState(1)
+  const [tripPlanned, setTripPlanned] = useState(false)
+  const [flyingFrom, setFlyingFrom] = useState("")
+  const [flyingTo, setFlyingTo] = useState("")
+  const [startDate, setStartDate] = useState("")
+  const [endDate, setEndDate] = useState("")
+  const [budget, setBudget] = useState(0)
+  const [weatherSummery, setWeatherSummery] = useState("")
+  const [flightSummery, setFlightSummery] = useState("")
+  const [flightLink, setFlightLink] = useState("")
+  const [hotelSummery, setHotelSummery] = useState("")
+  const [hotelLink, setHotelLink] = useState("")
+
+
+  async function GetTrip() {
+    const response = await fetch("http://localhost:3000/api/travelAssistant", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        numTravelers: numTravelers,
+        flyingFrom: flyingFrom,
+        flyingTo: flyingTo,
+        startDate: startDate,
+        endDate: endDate,
+        budget: budget
+      })
+    });
+
+    const data = await response.json();
+
+    const dataJSON = JSON.parse(data);
+
+    setWeatherSummery(dataJSON.weatherSummery)
+    setFlightSummery(dataJSON.flightSummery)
+    setFlightLink(dataJSON.flightLink)
+    setHotelSummery(dataJSON.hotelSummery)
+    setHotelLink(dataJSON.hotelLink)
+
+
+    setTripPlanned(true)
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="flex flex-col items-center justify-center h-screen overflow-auto">
+      {startScreen &&
+        <>
+          <Image src={Cat} height={50} width={380} alt="cat" />
+          <button onClick={() => setStartScreen(false)} className="min-w-[324px] min-h-[60px] rounded-2xl border-4 text-[25px] font-bold bg-[#4BDCB0]">Let's Begin</button>
+        </>
+      }
+      {!startScreen && !tripPlanned &&
+        <>
+          <h2 className="text-2xl font-bold mt-2">Number of travelers</h2>
+          <div className="w-[324px] border-4 rounded-4xl flex flex-row justify-between p-2 text-[25px] font-bold">
+            <button onClick={() => setNumTravelers(prev => prev - 1)} className="bg-black text-white w-[38px] h-[38px] text-[35px] font-bold rounded-full flex items-center justify-center">-</button>
+            {numTravelers}
+            <button onClick={() => setNumTravelers(prev => prev + 1)} className="bg-black text-white w-[38px] h-[38px] rounded-full text-[35px] font-bold flex items-center justify-center">+</button>
+          </div>
+          <h2 className="text-2xl font-bold mt-[49px]">Flying from</h2>
+          <div className="w-[324px] border-4 rounded-4xl flex flex-row justify-between p-2 text-[25px] font-bold">
+            <input onChange={e => setFlyingFrom(e.target.value)} value={flyingFrom} className="text-center w-full" />
+          </div>
+          <h2 className="text-2xl font-bold mt-2">Flying to</h2>
+          <div className="w-[324px] border-4 rounded-4xl flex flex-row justify-between p-2 text-[25px] font-bold">
+            <input value={flyingTo} onChange={e => setFlyingTo(e.target.value)} className="text-center w-full" />
+          </div>
+          <h2 className="text-2xl font-bold mt-[49px]">From date</h2>
+          <div className="w-[324px] border-4 rounded-4xl flex flex-row justify-between p-2 text-[25px] font-bold">
+            <input type="date" onChange={e => setStartDate(e.target.value)} value={startDate} className="text-center w-full" />
+          </div>
+          <h2 className="text-2xl font-bold mt-2">To date</h2>
+          <div className="w-[324px] border-4 rounded-4xl flex flex-row justify-between p-2 text-[25px] font-bold">
+            <input onChange={e => setEndDate(e.target.value)} value={endDate} type="date" className="text-center w-full" />
+          </div>
+          <h2 className="text-2xl font-bold mt-[38px]">Budget</h2>
+          <div className="w-[324px] border-4 rounded-4xl flex flex-row justify-between p-2 text-[25px] font-bold">
+            $
+            <input value={budget} onChange={e => setBudget(e.target.value)} type="number" className="text-center w-full" />
+          </div>
+          <button onClick={GetTrip} className="w-[324px] border-4 rounded-4xl flex flex-row justify-between p-2 text-[25px] font-bold flex items-center justify-center mt-[17px] bg-[#4BDCB0]">
+            Plan my trip!
+          </button>
+        </>
+      }
+
+      {!startScreen && tripPlanned &&
+
+        <>
+          <h1 className="text-5xl font-bold mt-20">Your Trip</h1>
+          <div className="flex flex-row gap-5 mt-[34px]">
+            <div className="bg-[#BBF7F7] min-h-[40px] min-w-[169px] rounded-3xl flex justify-center font-bold text-[20px] items-center">→ {startDate}</div>
+            <div className="bg-[#BBF7F7] min-h-[40px] min-w-[169px] rounded-3xl flex justify-center font-bold text-[20px] items-center">{endDate} ←</div>
+          </div>
+          <div className="bg-[#BBF7F7] min-h-[61px] min-w-[351px] mt-[25px] rounded-3xl flex justify-center font-bold text-[25px] items-center drop-shadow-2xl">{flyingFrom} → {flyingTo}</div>
+          <h1 className="text-3xl font-bold mt-[39px]">Weather</h1>
+          <div className="bg-[#BBF7F7] h-fit max-w-[351px] rounded-3xl flex justify-center text-[16px] items-center drop-shadow-2xl text-wrap p-[20px]">{weatherSummery}</div>
+          <h1 className="text-3xl font-bold mt-[35px]">Flights</h1>
+          <div className="bg-[#BBF7F7] h-fit max-w-[351px] rounded-3xl justify-center text-[16px] items-center drop-shadow-2xl flex flex-col text-wrap p-[20px]">{flightSummery}
+            <a href={flightLink} target="_blank"  rel="noopener noreferrer" className="w-[324px] h-[47px] border-4 rounded-4xl flex flex-row p-2 text-[25px] font-bold flex items-center justify-center mt-[10px] bg-[#4BDCB0]">
+              Book
+            </a>
+          </div>
+          <h1 className="text-3xl font-bold mt-[34px]">Hotel</h1>
+          <div className="bg-[#BBF7F7] h-fit max-w-[351px] rounded-3xl flex justify-center text-[16px] items-center drop-shadow-2xl flex flex-col text-wrap p-[20px]">{hotelSummery}
+             <a href={hotelLink} target="_blank"  rel="noopener noreferrer" className="w-[324px] h-[47px] border-4 rounded-4xl flex flex-row justify-between p-2 text-[25px] font-bold flex items-center justify-center mt-[10px] bg-[#4BDCB0]">
+              Book
+            </a>
+          </div>
+        </>
+
+      }
+
+    </main>
   );
 }
